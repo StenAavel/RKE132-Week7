c58c286ae896f1ecd74068df9fe343f2ddc509f4
Sten Aavel <stenaavel@gmail.com>
2022-12-01 17:44:16 +0200

    add week7 code

---
+++ Week7Functions/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out):");
+string UserChoice = Console.ReadLine();
+
+if(UserChoice == "in")
+{
+    PrintHello();
+} else
+{
+    PrintGoodbye();
+}
+
+PrintHello();
+PrintGoodbye();
+
+
+static void PrintHello() //function / method
+{
+    Console.WriteLine("Hello world!");
+}
+
+static void PrintGoodbye() 
+{
+    Console.WriteLine("See you later!");
+}
